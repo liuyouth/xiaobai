f9645bab8b598776b1a73c5035edafa85e6e226e
小白 <869689491@qq.com>
2017-09-17 11:44:01 +0800

    table列表加入

---
+++ src/main/java/com/company/project/core/TableResult.java
@@ -0,0 +1,60 @@
+package com.company.project.core;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * Created by mgxc2 on 2017/9/17.
+ */
+public class TableResult {
+    private int code;
+    private String message;
+    private Object data;
+    private int count;
+
+    public TableResult setCode(ResultCode resultCode) {
+        this.code = resultCode.code;
+        return this;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public TableResult setCode(int code) {
+        this.code = code;
+        return this;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public TableResult setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public TableResult setData(Object data) {
+        this.data = data;
+        return this;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public TableResult setCount(int count) {
+        this.count = count;
+        return this;
+    }
+
+
+    @Override
+    public String toString() {
+        return JSON.toJSONString(this);
+    }
+}
--- src/main/java/com/company/project/web/PhoneController.java
+++ src/main/java/com/company/project/web/PhoneController.java
@@ -1,14 +1,12 @@
 package com.company.project.web;
 import com.company.project.core.Result;
 import com.company.project.core.ResultGenerator;
+import com.company.project.core.TableResult;
 import com.company.project.model.Phone;
 import com.company.project.service.PhoneService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -48,6 +46,14 @@ public class PhoneController {
         List<Phone> list = phoneService.findAll();
         PageInfo pageInfo = new PageInfo(list);
         return ResultGenerator.genSuccessResult(pageInfo);
+    }
+
+    @GetMapping("/tablelist")
+    public TableResult tableist(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer limit) {
+        PageHelper.startPage(page, limit);
+        List<Phone> list = phoneService.findAll();
+        PageInfo pageInfo = new PageInfo(list);
+        return new TableResult().setCode(0).setCount(list.size()).setData(list);
     }
     @PostMapping("/addw")
     public Result addw(Phone phone) {
